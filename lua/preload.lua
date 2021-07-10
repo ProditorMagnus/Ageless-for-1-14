9aeb6090ea985cf4545934cee72ac7fcafed335d
ProditorMagnus <public.ravana@gmail.com>
2021-07-10 22:01:32 +0300

    add workaround for https://github.com/wesnoth/wesnoth/issues/5944

--- lua/preload.lua
+++ lua/preload.lua
@@ -7,6 +7,32 @@ local function sign(x)
 end
 local abs=math.abs
 
+-- workaround for https://github.com/wesnoth/wesnoth/issues/5944
+function wesnoth.wml_actions.store_time_of_day(cfg)
+	local x = tonumber(cfg.x)
+	local y = tonumber(cfg.y)
+
+	local turn = tonumber(cfg.turn)
+
+	if not turn or turn == 0 then
+		turn = wesnoth.current.turn
+	end
+
+	local variable = cfg.variable or "time_of_day"
+
+	local out
+	if x and y then
+		out = wesnoth.schedule.get_time_of_day(x, y, turn)
+	else
+		out = wesnoth.schedule.get_time_of_day(1, 1, turn)
+	end
+
+	for key, value in pairs(out) do
+		wml.variables[variable .. "[0]." .. key] = value
+	end
+end
+
+
 function wesnoth.wml_conditionals.AE_is_rpg()
 	local id = wesnoth.scenario.era.id or ""
 	id = id:lower()
