2000f1c02e5e1bd27bd334c3e1d3c5d47e43066e
ProditorMagnus <public.ravana@gmail.com>
2024-01-21 18:44:35 +0200

    changelog and version bump 4.32

--- lua/changelogs.lua
+++ lua/changelogs.lua
@@ -59,6 +59,7 @@ local message = {
             T.fire_event { name = "AE_show_changelog_menu" }
         }
     },
+	changelog("4.31"),
 	changelog("4.30"),
 	changelog("4.29"),
 	changelog("4.28"),
